71e88c73cdc516b169862c24279d125b15f5141e
Tareq <tareqaldhaifani@gmail.com>
2025-10-20 06:52:19 +0400

    Initial Commit

---
+++ Sawaed/SawaedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SawaedApp.swift
+//  Sawaed
+//
+//  Created by Tareq Aldhaifani on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SawaedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
